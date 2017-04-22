cccc624f288846de428e6f45019ee0dbb1174b4d
Lijing Wang <lijing@uchicago.edu>
2017-04-22 10:24:32 -0500

    378

--- Leetcode/kthSmallest.java
+++ Leetcode/kthSmallest.java
@@ -6,13 +6,13 @@ public class kthSmallest {
         used[0][0] = true;
         minheap.offer(new Cell(0, 0, matrix[0][0]));
         int result = matrix[0][0];
-        for(int i = 0; i < k; i++){
+        for(int i = 0; i < k; i++) {
             Cell cell = minheap.poll();
-            if (cell.row + 1 < n && used[cell.row + 1][cell.col] == false){
+            if (cell.row + 1 < n && used[cell.row + 1][cell.col] == false) {
                 minheap.offer(new Cell(cell.row + 1, cell.col, matrix[cell.row + 1][cell.col]));
                 used[cell.row + 1][cell.col] = true;
             }
-            if (cell.col + 1 < n && used[cell.row][cell.col + 1] == false){
+            if (cell.col + 1 < n && used[cell.row][cell.col + 1] == false) {
                 minheap.offer(new Cell(cell.row, cell.col + 1, matrix[cell.row][cell.col + 1]));
                 used[cell.row][cell.col + 1] = true;
             }
@@ -22,18 +22,18 @@ public class kthSmallest {
     }
 }
 
-class Cell implements Comparable<Cell>{
+class Cell implements Comparable<Cell> {
     int row;
     int col;
     int value;
-    public Cell(int row, int col, int value){
+    public Cell(int row, int col, int value) {
         this.col = col;
         this.row = row;
         this.value = value;
     }
     @Override
-    public int compareTo(Cell another){
-        if (this.value == another.value){
+    public int compareTo(Cell another) {
+        if (this.value == another.value) {
             return 0;
         }
         return this.value < another.value? -1:1;

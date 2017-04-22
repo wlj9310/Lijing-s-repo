41619ed227ce75ea6edd9e6632f319513702c728
Lijing Wang <lijing@uchicago.edu>
2017-04-22 16:52:20 -0500

    116

--- Leetcode/connect.java
+++ Leetcode/connect.java
@@ -9,36 +9,21 @@
 public class connect {
     public void connect(TreeLinkNode root) {
         Queue<TreeLinkNode> q1 = new LinkedList<TreeLinkNode>();
-        Queue<TreeLinkNode> q2 = new LinkedList<TreeLinkNode>();
         if (root == null) {
             return;
         }
         q1.add(root);
-        while (!q1.isEmpty() || !q2.isEmpty()) {
-            if (!q1.isEmpty()) {
-                while (!q1.isEmpty()) {
-                    TreeLinkNode cur = q1.remove();
-                    if (cur.left != null) {
-                        q2.add(cur.left);
-                    }
-                    if (cur.right != null) {
-                        q2.add(cur.right);
-                    }
-                    cur.next = q1.peek();
-                }
+        while (!q1.isEmpty()) {
+            TreeLinkNode cur = q1.remove();
+            if (cur.left == null) {
+                break;
             }
-            if (!q2.isEmpty()) {
-                while (!q2.isEmpty()) {
-                    TreeLinkNode cur = q2.remove();
-                    if (cur.left != null) {
-                        q1.add(cur.left);
-                    }
-                    if (cur.right != null) {
-                        q1.add(cur.right);
-                    }
-                    cur.next = q2.peek();
-                }
+            cur.left.next = cur.right;
+            if (cur.next != null) {
+                cur.right.next = cur.next.left;
             }
+            q1.add(cur.left);
+            q1.add(cur.right);
         }
     }
 }

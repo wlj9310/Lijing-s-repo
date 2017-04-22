4c23be245df542e74e776d9d675ce5164cf63aa3
Lijing Wang <lijing@uchicago.edu>
2017-04-22 02:07:31 -0500

    94

---
+++ Leetcode/inorderTraversal.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class inorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()){
+            if (cur != null){
+                stack.offerFirst(cur);
+                cur = cur.left;
+            } else {
+                cur = stack.pollFirst();
+                result.add(cur.val);
+                cur = cur.right;
+            }
+        }
+        return result;
+    }
+}

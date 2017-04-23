1b9d986fc0c227f971710d52a6b0f7261c6ed583
Lijing Wang <lijing@uchicago.edu>
2017-04-23 11:27:29 -0500

    33

---
+++ Leetcode/search.java
@@ -0,0 +1,26 @@
+public class search {
+    public int search(int[] nums, int target) {
+        if (nums == null) return -1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < nums[nums.length - 1]) {
+                if (nums[mid] < target && target <= nums[nums.length - 1]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                if (nums[0] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}

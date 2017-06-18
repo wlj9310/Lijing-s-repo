4d80af8024c6e30773b8ac57d10304ec80696064
Lijing Wang <lijing@uchicago.edu>
2017-06-17 19:55:14 -0500

    154

---
+++ Leetcode/findMin.java
@@ -0,0 +1,18 @@
+//154. Find Minimum in Rotated Sorted Array II
+public class findMin {
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else if (nums[mid] < nums[left]) {
+                right = mid;
+            } else {
+                right--;
+            }
+        }
+        return nums[left];
+    }
+} 
